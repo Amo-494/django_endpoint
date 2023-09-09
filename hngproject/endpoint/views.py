43c534f8c6c028e31b420d964c4024a2b161bfe8
Amo-494 <amoskipkirui0@gmail.com>
2023-09-09 18:52:49 +0300

    refining

--- hngproject/endpoint/views.py
+++ hngproject/endpoint/views.py
@@ -1,21 +1,37 @@
 from datetime import datetime
-from django.shortcuts import render
 from django.http import JsonResponse
 import pytz
 
-# Create your views here.
 def get_info(request):
-
+    # Retrieve query parameters from the request
     slack_name = request.GET.get('slack_name')
     track = request.GET.get('track')
 
+    # Check if required parameters are provided
     if not slack_name or not track:
         return JsonResponse({"error": "slack_name and track parameters are required."}, status=400)
     
+    # Get the current UTC time
     utc_now = datetime.now(pytz.utc)
     utc_time_str = utc_now.strftime("%Y-%m-%dT%H:%M:%SZ")
 
+    # Get the current day of the week
     current_day = utc_now.strftime("%A")
 
+    # Replace these hardcoded values with your actual GitHub repository URLs
     github_repo_url = "https://github.com/Amo-494/django_endpoint"
-    github_file_url = f"{github_repo_url}/blob/main/file_name.ext"
+    github_file_url = f"{github_repo_url}/blob/main/hngproject"
+
+    # Construct the JSON response
+    response = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time_str,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    # Return the JSON response with a 200 status code
+    return JsonResponse(response, status=200)
